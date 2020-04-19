e44f42a97280179f3935f34fc32e1f3a8915f482
coolish01 <watashun0191@gmail.com>
2020-04-20 05:20:06 +0900

    Update decode.py

--- decode.py
+++ decode.py
@@ -26,6 +26,8 @@ for arg in args.id:
         data[-1] += "1"
 
     print(data)
-    
+
     for d in data:
+      v = [d[i: i+8] for i in range(0, len(d), 8)]
+      print(v)
       print(format(int(d, 2), "x"))
